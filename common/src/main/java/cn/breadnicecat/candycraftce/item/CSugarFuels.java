30f28c32b163277b8dd394c7e21a8304c9846c4a
Bread_NiceCat <bread_nicecat@qq.com>
2024-10-04 12:55:24 +0800

    fix Sugar's datamap problem

--- common/src/main/java/cn/breadnicecat/candycraftce/item/CSugarFuels.java
+++ common/src/main/java/cn/breadnicecat/candycraftce/item/CSugarFuels.java
@@ -1,6 +1,8 @@
 package cn.breadnicecat.candycraftce.item;
 
+import cn.breadnicecat.candycraftce.utils.TickUtils;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.NotNull;
 
 import static cn.breadnicecat.candycraftce.item.CDataComponents.SUGAR_BURN_TIME;
@@ -8,11 +10,12 @@ import static cn.breadnicecat.candycraftce.item.CDataComponents.SUGAR_BURN_TIME;
 public class CSugarFuels {
 	
 	public static boolean isFuel(@NotNull ItemStack stack) {
-		return stack.has(SUGAR_BURN_TIME.get());
+		return stack.getItem() == Items.SUGAR || stack.has(SUGAR_BURN_TIME.get());
 	}
 	
 	public static int getBurnDuration(@NotNull ItemStack stack) {
 		if (stack.isEmpty()) return 0;
+		if (stack.getItem() == Items.SUGAR) return TickUtils.TICK_PER_SEC;
 		var v = stack.get(SUGAR_BURN_TIME.get());
 		return v != null ? v : 0;
 	}
